cada170ec18197238b38cd9a28015defbb23f0d6
chengfeifan <chengff11@gmail.com>
2021-07-20 14:20:03 +0800

    define class、

---
+++ reforming.py
@@ -0,0 +1,26 @@
+# impoort package
+import numpy as np
+
+class Reforming():
+    # 初始化函数
+    def __init__(self):
+        # 变量列表
+        reactor_size = self.reactor_size
+    # 微分方程
+    def reformingModel(F,Pressure,T):
+        # 微分方程
+        ## 浓度
+        ## 温度
+        ## 压力
+    
+    # 迭代求解
+    def reformingIter(E0):
+        # 边界条件
+
+
+if __name__ == "__main__":   
+    reforming = Reform()
+    reforming.reformingModel()
+    reforming.reformingIter()
+
+
